94a2d26e603d33c9695719598c934bcd8e0c791e
ifiok <ifiokakpanm3@gmail.com>
2024-02-06 08:12:02 -0500

    minor edit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           I want to laern more 
         </a>
       </header>
     </div>
